c8c7d7fff66158b582679dc187b4dec963d8d6f0
Ayfri <pierre.ayfri@gmail.com>
2023-03-01 17:23:52 +0100

    feat(ennemy): Change ennemy path, fix compile error.

--- assets/scripts/power_up.gd
+++ assets/scripts/power_up.gd
@@ -2,27 +2,23 @@ extends Area2D
 
 @export var speed = 300
 @export var effect_duration = 5
-@export var sprite = $Sprite2D
-@export var click_area = $ClickArea
-
 
 func _ready():
 	position.x = randf_range(400,1200)
 
-
 func _process(delta):
 	position.y += speed * delta
 	if position.y > 900:
-		position.y = -200	
+		position.y = -200
 		position.x = randf_range(400,1200)
 		visible = true
 
 func apply_powerup_effect():
 		EnemyStats.runspeed = 400;
-		await get_tree().create_timer(2.0).timeout		
+		await get_tree().create_timer(2.0).timeout
 		EnemyStats.runspeed = 20;
-		
+
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		visible = false 
+		visible = false
 		apply_powerup_effect()
