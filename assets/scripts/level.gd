15ffe05e8f1781851ea2810f4299793dbac39826
AlexandreRocchi <alexandre.rocchi@mailo.com>
2023-03-29 15:52:44 +0200

    feat(powerup): fix speed and add debuff

--- assets/scripts/level.gd
+++ assets/scripts/level.gd
@@ -2,8 +2,11 @@ class_name Level;
 extends Node2D;
 
 const starting_money := 30;
+const starting_health := 10;
 
+@export var health := starting_health;
 @export var money := starting_money;
+
 @onready var map := $Map as Map;
 
 
--- assets/scripts/level_interface.gd
+++ assets/scripts/level_interface.gd
@@ -1,12 +1,18 @@
 extends CanvasLayer;
 
-@onready var scoreLabel = $RichTextLabel as RichTextLabel;
+@onready var money_label := $MoneyDisplay as RichTextLabel;
+@onready var health_label := $HealthDisplay as RichTextLabel;
 
 func _process(_delta: float) -> void:
 	if Globals.level == null: return;
 
-	var text = """
+	var money_label_text := """
 		[img=40x40]assets/sprites/ui/coin_cat.png[/img][font_size=42]%s[/font_size]
 	""".strip_edges();
 
-	scoreLabel.text = text % Globals.level.money;
+	var health_label_text := """
+		[font_size=42]%s[/font_size]
+	""".strip_edges();
+
+	money_label.text = money_label_text % Globals.level.money;
+	health_label.text = health_label_text % Globals.level.health;
