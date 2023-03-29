2c402fc71cb80abd7b653b1e5ef9b83c075db3fc
Ayfri <pierre.ayfri@gmail.com>
2023-03-29 17:49:31 +0200

    fix(ennemies): Fix merge.

--- assets/scripts/enemy.gd
+++ assets/scripts/enemy.gd
@@ -1,18 +1,31 @@
 class_name Enemy;
 extends Area2D
 
+
 var health := 10.0;
 var money_reward := 5.0;
 
+@onready var animated_sprite := $AnimatedSprite2D as AnimatedSprite2D;
+@onready var parent := get_parent() as PathFollow2D;
+@onready var previous_point := Vector2(parent.position);
 
 func _process(delta: float) -> void:
-	var parent := get_parent() as PathFollow2D;
 	parent.progress = parent.progress + Globals.enemy_speed * delta;
+
+	var x_pos_difference := parent.position.x - previous_point.x;
+	var y_pos_difference := parent.position.y - previous_point.y;
+
+	previous_point = Vector2(parent.position);
+
+	if absf(x_pos_difference) > absf(y_pos_difference):
+		animated_sprite.play("walk_right" if x_pos_difference > 0 else "walk_left");
+	else:
+		animated_sprite.play("walk_down" if y_pos_difference > 0 else "walk_up");
 
 	if health <= 0:
 		Globals.level.money += money_reward;
-		queue_free();
+		parent.queue_free();
 
 	if parent.progress_ratio == 1:
 		Globals.level.health -= 1;
-		queue_free();
+		parent.queue_free();
