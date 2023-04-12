0202a88f1e9e72cbd56d85537dc1a74d826e7f55
Ayfri <pierre.ayfri@gmail.com>
2023-04-12 17:44:30 +0200

    feat(pause menu): Add pause menu.

--- assets/scripts/level.gd
+++ assets/scripts/level.gd
@@ -1,6 +1,7 @@
 class_name Level;
 extends Node2D;
 
+const PauseScene := preload("res://scenes/pause_interface.tscn");
 const starting_money := 30;
 const starting_health := 10;
 
@@ -9,6 +10,25 @@ const starting_health := 10;
 
 @onready var map := $Map as Map;
 
+var pause_scene: PauseMenu = null;
+
 
 func _ready() -> void:
 	Globals.level = self;
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed('Escape Menu'):
+		toggle_pause();
+
+
+func toggle_pause() -> void:
+	map.get_tree().paused = !map.get_tree().paused;
+
+	if map.get_tree().paused:
+		pause_scene = PauseScene.instantiate() as PauseMenu;
+		pause_scene.transform.origin = get_viewport_transform().get_scale() / 2;
+		add_child(pause_scene);
+
+	elif pause_scene != null:
+		remove_child(pause_scene);
---
+++ assets/scripts/pause.gd
@@ -0,0 +1,22 @@
+class_name PauseMenu;
+extends CanvasLayer;
+
+
+
+func _on_return_button_pressed() -> void:
+	Globals.level.toggle_pause();
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level.tscn");
+	Globals.level.toggle_pause();
+
+
+func _on_quit_to_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn");
+	Globals.level.toggle_pause();
+
+
+func _on_exit_to_desktop_button_pressed() -> void:
+	get_tree().quit();
+
