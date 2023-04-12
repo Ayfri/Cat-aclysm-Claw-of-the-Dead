611c6f7e85ee7ae53903eca366df6e95090bc46d
MateoPerrotNasi <bew1432@gmail.com>
2023-04-12 09:18:14 +0200

    refactor(unused file): delete an unused scenes

--- assets/scripts/spawner.gd
+++
@@ -1,8 +0,0 @@
-class_name Spawner;
-extends Node2D;
-
-const EnemyScene = preload("res://scenes/enemy.tscn");
-
-func _on_spawn_interval_timeout() -> void:
-	var enemy_instance = EnemyScene.instantiate();
-	$Path2D.add_child(enemy_instance);
