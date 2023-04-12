afa288eec38766ab9f21259d8ea1bc07c2fcdab2
AlexandreRocchi <alexandre.rocchi@mailo.com>
2023-04-12 16:35:03 +0200

    feat(menu): fix menu

--- assets/scripts/menu.gd
+++ assets/scripts/menu.gd
@@ -1,19 +1,9 @@
 extends Control
 
 const option = preload("res://scenes/option.tscn");
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/map.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
 
 
 func _on_options_button_pressed():
@@ -23,4 +13,3 @@ func _on_options_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
-	pass # Replace with function body.
