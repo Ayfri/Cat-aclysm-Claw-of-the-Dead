9f845b519b1cb30dd9cc53005b09b371523eb9ee
Ayfri <pierre.ayfri@gmail.com>
2023-04-29 21:57:22 +0200

    fix(level gui): Fix health/money texts being blurry.

--- assets/scripts/level_interface.gd
+++ assets/scripts/level_interface.gd
@@ -7,12 +7,12 @@ func _process(_delta: float) -> void:
 	if Globals.level == null: return;
 
 	var money_label_text := """
-		[img=40x40]assets/sprites/ui/coin_cat.png[/img] [font_size=42]%s[/font_size]
+		[img=40x40]assets/sprites/ui/coin_cat.png[/img] [font_size=41]%s[/font_size]
 	""".strip_edges();
-	
+
 
 	var health_label_text := """
-		[img=40x40]assets/sprites/ui/heart_cat.png[/img] [font_size=42]%s[/font_size]
+		[img=40x40]assets/sprites/ui/heart_cat.png[/img] [font_size=41]%s[/font_size]
 	""".strip_edges();
 
 	money_label.text = money_label_text % Globals.level.money;
