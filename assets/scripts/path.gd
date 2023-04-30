02cf7c383a9b9a5e7827871d66690aaea5eed646
Ayfri <pierre.ayfri@gmail.com>
2023-04-30 22:54:49 +0200

    fix(path): Fix condition for passing to next wave.

--- assets/scripts/path.gd
+++ assets/scripts/path.gd
@@ -1,10 +1,10 @@
 extends Node2D;
 
-var current_wave = 0;
-var mobs_to_spawn = 0;
+var current_wave := 0;
+var mobs_to_spawn := 0;
 
-#Always put even numbers
-var wave_mobs = [2,10,20,30,40,50,60];
+# Waves should only contains an even number of monsters.
+var wave_mobs = [2, 10, 20, 30, 40, 50, 60];
 
 const EnemyScene := preload("res://scenes/ennemies/enemy_big.tscn");
 @onready var pre_wave_timer := $PreWaveTimer as Timer;
@@ -16,23 +16,24 @@ const EnemyScene := preload("res://scenes/ennemies/enemy_big.tscn");
 		return array;
 
 
-func _process(delta):
-	if mobs_to_spawn == 0 && paths.all(func(path: Path2D): path.get_child_count() < 1) :
+func _process(_delta: float):
+	if mobs_to_spawn == 0 && paths.all(func(path: Path2D): return path.get_child_count() < 1):
 		if pre_wave_timer.is_stopped():
 			pre_wave_timer.start();
 
 
 func _on_pre_wave_timer_timeout():
 	if current_wave <= len(wave_mobs):
-		current_wave += 1;
-		mobs_to_spawn += wave_mobs[current_wave-1];
+		mobs_to_spawn += wave_mobs[current_wave];
 		enemy_spawn_timer.start();
+		current_wave += 1;
 
 
 func _on_enemy_spawn_timer_timeout():
 	var path_follow := EnemyScene.instantiate() as PathFollow2D;
 	path_follow.get_node('Enemy').stats = Globals.enemy_stats[1];
 	paths.pick_random().add_child(path_follow);
+
 	mobs_to_spawn -= 1;
 	if mobs_to_spawn > 0:
 		enemy_spawn_timer.start();
