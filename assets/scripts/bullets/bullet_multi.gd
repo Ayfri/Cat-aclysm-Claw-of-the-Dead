91a5fa97cf9bec0acd69b71caa7a9d218400fc17
Ayfri <pierre.ayfri@gmail.com>
2023-05-01 17:50:56 +0200

    feat(bullet_single): Change bottle particles to colored when bullet is poisonous.

--- assets/scripts/bullets/bullet_multi.gd
+++ assets/scripts/bullets/bullet_multi.gd
@@ -7,6 +7,14 @@ extends IBullet;
 @onready var particles_emitter := $GPUParticles2D as GPUParticles2D;
 
 
+## Changes the animation to take colored sprites when bullet is poisonous.
+func _ready() -> void:
+	if tower.upgraded:
+		var particle_material := particles_emitter.process_material as ParticleProcessMaterial;
+		particle_material.anim_offset_min = 0.5;
+		particle_material.anim_offset_max = 1;
+
+
 func physics() -> void:
 	velocity = global_position.direction_to(target.global_position) * speed;
 	rotation_degrees += 12;
