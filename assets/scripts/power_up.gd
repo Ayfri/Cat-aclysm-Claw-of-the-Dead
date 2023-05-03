e288b4b687631ec0a79412d1d0b8eddd4bbbcc37
MateoPerrotNasi <bew1432@gmail.com>
2023-05-04 01:31:50 +0200

    fix(power-up): fix collision shape, sound and delete unused timer

--- assets/scripts/power_up.gd
+++ assets/scripts/power_up.gd
@@ -3,6 +3,10 @@ extends Area2D;
 
 const sprite_buff := preload("res://assets/sprites/powers/buff.png");
 const sprite_debuff := preload("res://assets/sprites/powers/debuff.png")
+
+@onready var sprite := $Sprite2D as Sprite2D;
+@onready var audio_stream_player := $AudioStreamPlayer as AudioStreamPlayer;
+
 @export var speed := 300;
 @export var effect_duration := 5;
 @export var random_spawn := 1;
@@ -21,13 +25,13 @@ func _on_timer_sprite_timeout():
 	random_spawn = randi() % 3 + 1;
 
 	if random_spawn == 3:
-		$Sprite2D.texture = sprite_debuff;
+		sprite.texture = sprite_debuff;
 	else:
-		$Sprite2D.texture = sprite_buff;
+		sprite.texture = sprite_buff;
 
 
 func apply_powerup_effect() -> void:
-	if $Sprite2D.texture == sprite_debuff:
+	if sprite.texture == sprite_debuff:
 		Globals.enemy_speed_multiplier = 2;
 		await get_tree().create_timer(2.0).timeout;
 		Globals.enemy_speed_multiplier = 1;
@@ -39,6 +43,6 @@ func apply_powerup_effect() -> void:
 
 func _on_input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		audio_stream_player.play();
 		visible = false;
-		$AudioStreamPlayer.play()
 		apply_powerup_effect();
