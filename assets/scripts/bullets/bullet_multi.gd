ad7b512e2b5f835dc44c559d9288a92aace7a69e
Ayfri <pierre.ayfri@gmail.com>
2023-05-05 01:09:38 +0200

    fix(bullets): Fix bullets not finding a target being deleted, add particles on the ground when bottle explodes.

--- assets/scripts/bullets/bullet_multi.gd
+++ assets/scripts/bullets/bullet_multi.gd
@@ -4,30 +4,32 @@ extends IBullet;
 
 @export var poison_duration := 2.0;
 
-@onready var particles_emitter := $GPUParticles2D as GPUParticles2D;
+@onready var explosion_particles := $ExplosionParticles as GPUParticles2D;
+@onready var ground_particles := $GroundParticles as GPUParticles2D;
 @onready var hitbox := $Area2D/CollisionShape2D as CollisionShape2D;
 
 
 ## Changes the animation to take colored sprites when bullet is poisonous.
 func _ready() -> void:
 	if tower.upgraded:
-		var particle_material := particles_emitter.process_material as ParticleProcessMaterial;
+		var particle_material := explosion_particles.process_material as ParticleProcessMaterial;
+		particle_material = particle_material.duplicate();
 		particle_material.anim_offset_min = 0.5;
 		particle_material.anim_offset_max = 1;
+		explosion_particles.process_material = particle_material;
+
+		particle_material = ground_particles.process_material as ParticleProcessMaterial;
+		particle_material = particle_material.duplicate();
+		particle_material.anim_offset_min = 0.5;
+		particle_material.anim_offset_max = 1;
+		ground_particles.process_material = particle_material;
 
 
-func physics() -> void:
-	velocity = global_position.direction_to(target.global_position) * speed;
-	rotation_degrees += 12;
-
-
-func on_destroy(enemy: IEnemy) -> void:
-	if tower.upgraded:
-		enemy.poison(tower, poison_duration, tower.stats.upgrade_damages);
-
+func bullet_destroy_effects() -> void:
 	if !sprite.visible: return;
 	sprite.hide();
-	position = enemy.global_position;
+
+	rotation = 0;
 
 	if !hitbox.disabled:
 		var hitbox_shape := hitbox.shape as CircleShape2D;
@@ -35,11 +37,35 @@ func on_destroy(enemy: IEnemy) -> void:
 		hitbox_shape.radius *= 3;
 		hitbox.set_deferred("shape", hitbox_shape);
 
-	particles_emitter.emitting = true;
-	particles_emitter.z_index = 1000;
+	explosion_particles.emitting = true;
+	explosion_particles.z_index = 1000;
+
+	ground_particles.emitting = true;
+	ground_particles.z_index = 1000;
 
 	await get_tree().create_timer(0.3).timeout;
 	hitbox.disabled = true;
 
 	await get_tree().create_timer(1.0).timeout;
 	queue_free();
+
+
+func physics() -> void:
+	if target != null:
+		direction = global_position.direction_to(target.global_position);
+
+	if direction == Vector2(0, 0):
+		direction = Vector2.from_angle(randi_range(0, 360));
+
+	velocity = direction * speed;
+	rotation_degrees += 12;
+
+
+func on_destroy(enemy: IEnemy) -> void:
+	if tower.upgraded:
+		enemy.poison(tower, poison_duration, tower.stats.upgrade_damages);
+
+	if sprite.visible: position = enemy.global_position;
+
+	bullet_destroy_effects();
+
--- assets/scripts/bullets/i_bullet.gd
+++ assets/scripts/bullets/i_bullet.gd
@@ -7,15 +7,21 @@ var speed: int;
 var target: IEnemy;
 var tower: ITower;
 
+@onready var direction := Vector2();
 @onready var sprite := $Sprite2D as Sprite2D;
 
 
 func _physics_process(_delta: float) -> void:
-	if target == null:
-		queue_free();
-		return;
-
 	if !sprite.visible: return;
+	if target != null && target.is_dead: target = null;
+
+	if target == null:
+		var distance_from_origin_vec := tower.global_position - global_position;
+		var distance_from_origin := distance_from_origin_vec.abs().length()
+
+		if distance_from_origin > 1500:
+			queue_free();
+
 	physics();
 	move_and_slide();
 
@@ -23,22 +29,29 @@ func _physics_process(_delta: float) -> void:
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area is IEnemy:
 		var enemy := area as IEnemy;
-		if enemy.is_dead:
-			queue_free();
-			return;
+		if enemy.is_dead: return;
 
 		enemy.on_hit.emit(tower, damages);
 		on_destroy(enemy as IEnemy);
 
 
-func on_destroy(enemy: IEnemy) -> void:
+func bullet_destroy_effects() -> void:
+	queue_free();
+
+
+func on_destroy(_enemy: IEnemy) -> void:
 	queue_free();
 
 
 func physics() -> void:
-	velocity = global_position.direction_to(target.global_position) * speed;
-	look_at(target.global_position);
+	if target != null:
+		direction = global_position.direction_to(target.global_position);
+		look_at(target.global_position);
 
+	if direction == Vector2(0, 0):
+		direction = Vector2.from_angle(randi_range(0, 360));
+
+	velocity = direction * speed;
 
 func set_sprite_texture(texture: Texture2D) -> void:
 	sprite.texture = texture;
