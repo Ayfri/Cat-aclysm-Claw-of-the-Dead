0bc0140249db8e20ef67519ff2f322bf931ed967
AlexandreRocchi <alexandre.rocchi@mailo.com>
2023-05-10 13:57:24 +0200

    feat(pigeon): Rotate the pigeon

---
+++ assets/scripts/pigeon.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var pigeon := $AnimatedSprite2D as AnimatedSprite2D;
+@onready var audio_stream_player := $AudioStreamPlayer as AudioStreamPlayer;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	audio_stream_player.play();
+	pigeon.position.x += 130 * delta;
+	if pigeon.position.x > 5000:
+		pigeon.position.x = 0;
+
