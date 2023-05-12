65a683f1cc0a0346b937762748859f395d5a4295
Ayfri <pierre.ayfri@gmail.com>
2023-05-12 22:54:20 +0200

    feat(debug): Add debug key for giving money.

--- assets/scripts/debug.gd
+++ assets/scripts/debug.gd
@@ -6,7 +6,6 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if Globals.level.finished || !event is InputEventKey: return;
 	var key_event := event as InputEventKey;
 
-
-
 	if key_event.as_text_physical_keycode() == "Z": Globals.level.win();
 	if key_event.as_text_physical_keycode() == "X": Globals.level.lose();
+	if key_event.as_text_physical_keycode() == "Q": Globals.level.money += 5;
